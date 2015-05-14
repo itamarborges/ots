8827fce67b6d86dd64aa403c90f69ef44908d596
gabriela <gbizotto@gmail.com>
2015-05-14 18:26:27 -0300

    Calendário funcionando nos filtros - ainda sem formatação

---
+++ app/src/main/java/br/borbi/ots/FiltersActivity.java
@@ -0,0 +1,106 @@
+package br.borbi.ots;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+public class FiltersActivity extends Activity {
+
+    private static DateFormat dateFormat;
+    private static TextView dateView;
+    private static String dateBeginString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+
+        dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+
+        dateView = (TextView) findViewById(R.id.textViewDateBeginPeriod);
+
+        final Button datePickerButton = (Button) findViewById(R.id.date_picker_button);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_filters, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showCalendar(View view) {
+        showDatePickerDialog();
+    }
+
+    // DialogFragment used to pick a ToDoItem deadline date
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            // Use the current date as the default date in the picker
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            setDateString(year, monthOfYear, dayOfMonth);
+
+            dateView.setText(dateBeginString);
+        }
+    }
+
+    private void showDatePickerDialog() {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    /*
+    Formata a data marcada no calendário de acordo com o formato marcado como padrão no aparelho.
+     */
+    private static void setDateString(int year, int monthOfYear, int dayOfMonth) {
+        Calendar data = new GregorianCalendar(year,monthOfYear,dayOfMonth);
+        dateBeginString = dateFormat.format(data.getTime());
+    }
+}
