c9017bf49182b4f0f754d6c44106a158c383a2fc
gabriela <gbizotto@gmail.com>
2015-07-13 18:26:09 -0300

    Remoção menu de todas as telas.

--- app/src/main/java/br/borbi/ots/FailureActivity.java
+++ app/src/main/java/br/borbi/ots/FailureActivity.java
@@ -42,28 +42,6 @@ public class FailureActivity extends ActionBarActivity implements GoogleApiClien
         setContentView(R.layout.activity_failure);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_failure, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void tryToFindMeClick(View v) {
 
         bTriedToConnect = false;
