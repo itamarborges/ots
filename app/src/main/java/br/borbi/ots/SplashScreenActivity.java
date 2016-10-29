084e3ccfcc37125bac095fcb6e3ad506667ba8c3
itamarborges <itamarborges@gmail.com>
2016-10-29 18:39:32 -0200

    Agilizando a SplashScreen

--- app/src/main/java/br/borbi/ots/SplashScreenActivity.java
+++ app/src/main/java/br/borbi/ots/SplashScreenActivity.java
@@ -2,25 +2,11 @@ package br.borbi.ots;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 import br.borbi.ots.utility.ForwardUtility;
 import br.borbi.ots.utility.LocationUtility;
-import br.borbi.ots.utility.Utility;
 
 
 public class SplashScreenActivity extends Activity  {
@@ -34,27 +20,10 @@ public class SplashScreenActivity extends Activity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-
         mContext = this;
 
         LocationUtility.cleanSavedCoordinates(this);
 
-        mImgSplash = (ImageView) findViewById(R.id.imageView);
-
-        int img = ((int) (Math.random() * 10)) ;
-
-        if (img % 2 == 0) {
-            mImgSplash.setBackgroundResource(R.drawable.logo_novo_par); ;
-        } else {
-            mImgSplash.setBackgroundResource(R.drawable.logo_novo_impar);
-        }
-
-        new Timer().schedule(new TimerTask() {
-
-            public void run() {
-                ForwardUtility.goToFilters(mContext,true);
-            }
-        }, TIME_SPLASH);
+        ForwardUtility.goToFilters(mContext,true);
     }
 }
