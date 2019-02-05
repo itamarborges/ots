06db8136a7186223b9e1159bab511fd25f796306
Itamar Borges da Silva Filho <itamarborges@gmail.com>
2019-02-05 11:26:47 -0200

    Removing menu from resultActivity

--- app/src/main/java/br/borbi/ots/ResultActivity.java
+++ app/src/main/java/br/borbi/ots/ResultActivity.java
@@ -46,13 +46,6 @@ public class ResultActivity extends AppCompatActivity implements GoogleApiClient
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_filters, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
